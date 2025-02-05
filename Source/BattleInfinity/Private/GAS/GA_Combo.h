010fdbbd5d96d8d8326819368813c149abfcb37e
Li <jili1@uiwtx.edu>
2025-02-05 11:44:18 -0600

    Added comobo ability class

---
+++ Source/BattleInfinity/Private/GAS/GA_Combo.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/BGameplayAbility.h"
+#include "GA_Combo.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UGA_Combo : public UBGameplayAbility
+{
+	GENERATED_BODY()
+public:	
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
