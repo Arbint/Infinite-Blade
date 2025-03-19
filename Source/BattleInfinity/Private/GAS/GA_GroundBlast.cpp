a006e808ec068eaade6d48b84cc8fcb389f44951
Li <jili1@uiwtx.edu>
2025-03-19 11:37:29 -0500

    Ground Blast Ability Given

---
+++ Source/BattleInfinity/Private/GAS/GA_GroundBlast.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GAS/GA_GroundBlast.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+
+void UGA_GroundBlast::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	if (!HasAuthorityOrPredictionKey(ActorInfo, &ActivationInfo))
+		return;
+
+	UAbilityTask_PlayMontageAndWait* PlayCastMontage = UAbilityTask_PlayMontageAndWait::CreatePlayMontageAndWaitProxy(this,
+		NAME_None, CastMontage);
+
+	PlayCastMontage->OnBlendOut.AddDynamic(this, &UGA_GroundBlast::K2_EndAbility);
+	PlayCastMontage->OnCancelled.AddDynamic(this, &UGA_GroundBlast::K2_EndAbility);
+	PlayCastMontage->OnInterrupted.AddDynamic(this, &UGA_GroundBlast::K2_EndAbility);
+	PlayCastMontage->OnCompleted.AddDynamic(this, &UGA_GroundBlast::K2_EndAbility);
+
+	PlayCastMontage->ReadyForActivation();
+}
