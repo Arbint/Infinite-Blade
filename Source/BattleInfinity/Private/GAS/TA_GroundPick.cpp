11980a85dc40af00fe2a10d9a7dabfdec93d676e
Li <jili1@uiwtx.edu>
2025-03-24 12:07:59 -0500

    Taget actor now moves to view point

--- Source/BattleInfinity/Private/GAS/TA_GroundPick.cpp
+++ Source/BattleInfinity/Private/GAS/TA_GroundPick.cpp
@@ -8,3 +8,37 @@ ATA_GroundPick::ATA_GroundPick()
 {
 	PrimaryActorTick.bCanEverTick = true;
 }
+
+void ATA_GroundPick::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	if (PrimaryPC && PrimaryPC->IsLocalPlayerController())
+	{
+		SetActorLocation(GetTargetingAimLoc());
+	}
+}
+
+FVector ATA_GroundPick::GetTargetingAimLoc() const
+{
+	if(!PrimaryPC)
+		return GetActorLocation();
+	
+	FVector ViewLoc;
+	FRotator ViewRot;
+	PrimaryPC->GetPlayerViewPoint(ViewLoc, ViewRot);
+	FVector TraceEnd = ViewLoc + ViewRot.Vector() * TargetMaxDistance;
+
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, ViewLoc, TraceEnd, ECC_Visibility);
+	if (!HitResult.bBlockingHit)
+	{
+		GetWorld()->LineTraceSingleByChannel(HitResult, 
+			TraceEnd, 
+			TraceEnd + FVector::DownVector * TNumericLimits<float>::Max(), ECC_Visibility);
+	}
+
+	if (HitResult.bBlockingHit)
+		return HitResult.ImpactPoint;
+
+	return GetActorLocation();
+}
--- Source/BattleInfinity/Private/GAS/TA_GroundPick.h
+++ Source/BattleInfinity/Private/GAS/TA_GroundPick.h
@@ -15,4 +15,11 @@ class ATA_GroundPick : public AGameplayAbilityTargetActor
 	GENERATED_BODY()
 public:		
 	ATA_GroundPick();
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
+	FVector GetTargetingAimLoc() const;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Targeting")
+	float TargetMaxDistance = 2000.f;
 };
